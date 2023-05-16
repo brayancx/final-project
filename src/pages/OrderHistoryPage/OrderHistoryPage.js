2e11e4f48fec76b180635304a2d371e51fe627a0
Jeff <jeff_lim408@icloud.com>
2023-05-16 14:14:47 -0500

    mern_stack INFRASTRUCTURE

---
+++ src/pages/OrderHistoryPage/OrderHistoryPage.js
@@ -0,0 +1,17 @@
+import { checkToken } from '../../utilities/users-service'
+
+export default function OrderHistoryPage() {
+
+  const handleCheckToken = async () => {
+    const expDate = await checkToken();
+    alert(expDate);
+  }
+
+
+  return (
+    <div>
+      <h1>OrderHistoryPage</h1>
+      <button onClick={handleCheckToken}>Check When My Login Expires</button>
+    </div>
+  )
+}
